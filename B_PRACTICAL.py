2538a9ae4ccb40eee3396cedd7e2d1c5e1188624
Tejas-2005 <106252704+Tejas-2005@users.noreply.github.com>
2022-11-23 18:58:29 +0530

    Add files via upload

---
+++ B_PRACTICAL.py
@@ -0,0 +1,30 @@
+import pickle as p 
+def search():
+    file = open(".\King.txt","rb")
+    data = {}
+    search = int(input("Enter which roll no. to search : "))
+    try:
+        while True:
+            data = p.load(file)
+            # print(data)
+            if data['Roll no']==search:
+                print(data)
+    except:
+        file.close()
+
+def update_marks():
+    file = open(".\King.txt","r+b")
+    a = {}
+    roll = int(input("Enter roll no whose marks to be updated : "))
+    marks = int(input("Enter marks updation you want : "))
+    try:
+        while True:
+            a = p.load(file)
+            if a['Roll no'] == roll:
+                print(a)
+                a['marks'] = marks
+                print(a)
+    except:
+        file.close()
+
+
